77438261b3a584e442ac20632749a3f793c615f8
Kibwana Miruru <mirukibs12.km@gmail.com>
2026-07-30 23:31:58 +0300

    feat(VenueReservation): scaffolded the reservation aggregate.
    - Implemented attributes
    - Added method signatures
    - Accounted for the reservation status enum in the plantuml

--- src/main/java/com/github/mirukibs/venturerequest/contexts/venuereservation/domain/reservationaggregate/Reservation.java
+++ src/main/java/com/github/mirukibs/venturerequest/contexts/venuereservation/domain/reservationaggregate/Reservation.java
@@ -1,4 +1,20 @@
 package com.github.mirukibs.venturerequest.contexts.venuereservation.domain.reservationaggregate;
 
+import com.github.mirukibs.venturerequest.contexts.venuereservation.domain.sharedkernel.TimePeriod;
+import lombok.Getter;
+
 public class Reservation {
+    private String id;
+    @Getter
+    private String venueId;
+    @Getter
+    private String venueRequestId;
+    @Getter
+    private TimePeriod timePeriod;
+    @Getter
+    private ReservationStatus status;
+
+    public void cancel() {}
+
+    public void confirm() {}
 }
