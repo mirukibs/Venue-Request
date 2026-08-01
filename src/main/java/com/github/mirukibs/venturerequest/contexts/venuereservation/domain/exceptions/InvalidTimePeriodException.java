6fc9ab92f9db2e85d9c5f4942da7ac1f8c3ebc5d
Kibwana Miruru <mirukibs12.km@gmail.com>
2026-08-01 03:33:41 +0300

    feat(VenueReservation): Concrete implementation of the TimePeriod value object.

---
+++ src/main/java/com/github/mirukibs/venturerequest/contexts/venuereservation/domain/exceptions/InvalidTimePeriodException.java
@@ -0,0 +1,7 @@
+package com.github.mirukibs.venturerequest.contexts.venuereservation.domain.exceptions;
+
+public class InvalidTimePeriodException extends DomainException {
+    public InvalidTimePeriodException(String message) {
+        super(message);
+    }
+}
